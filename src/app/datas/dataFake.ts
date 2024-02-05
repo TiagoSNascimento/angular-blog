7bf4a751fa6e0d5cd4f5106e30983311664a70b3
TiagoSNascimento <jamesnascwhite@hotmail.com>
2024-02-04 21:20:45 -0300

    content created

---
+++ src/app/datas/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "NEW MAN OF FERRO EM 3D",
+        "description": "Marvel anuncia um novo filme do Man of Ferro, confira!",
+        "photoCover":"https://legadodamarvel.com.br/wp-content/uploads/2023/10/Tony-Stark-o-Homem-de-Ferro-morreu-oficialmente-hoje-no-MCU-legadodamarvel.webp"
+    },
+
+    {
+        "id": "2",
+        "title": "Nova Série anunciada no Disney Plus+",
+        "description": "Confira os trailers!",
+        "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    },
+
+    {
+        "id": "3",
+        "title": "Novo filme do Pantera Negra lançado em breve",
+        "description": "Confira os trailer!",
+        "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/F6482D58C8BB4D899FA4682BF27652C3E1BB73F5971E5F963994112F26A73690/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    },
+
+    {
+        "id": "4",
+        "title": "Novo filme do X-Men está em produção, afirma diretor",
+        "description": "Confira os trailer!",
+        "photoCover":"https://s2-techtudo.glbimg.com/XEjoJ9NZIfzAak4yzk_nxl-urtI=/0x0:2560x1600/924x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/o/o/RWzATXSaG30IWVkdLHwQ/x-men-capa.jpg"
+    },
+
+]
